8ae2d0774cd4adb3b8c79519e406f6bde0baeb63
maria-magdalena-bogoeva <maggiiiiii@abv.bg>
2023-01-11 09:29:20 +0200

    asp

---
+++ WebAuto/WebAuto/Models/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAuto.Models
+{
+    public class Client
+    {
+        public Client()
+        {
+            this.Purchases = new HashSet<Purchase>();
+        }
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+        [Required]
+        [MinLength(10)]
+        [MaxLength(10)]
+        public string EGN { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Adress { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        public ICollection<Purchase> Purchases { get; set; }
+    }
+}
